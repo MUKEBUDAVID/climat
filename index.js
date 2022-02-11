65a70800ae39b39f4d4e7ff2c6ebd69f9189c2fc
MUKEBUDAVID <davidmukebu@gmail.com>
2022-02-11 22:04:16 +0100

    first party

---
+++ index.js
@@ -0,0 +1,35 @@
+// let span = document.querySelectorAll("span");
+// let btn = document.getElementsByTagName("button");
+// let temp = 10;
+// let zemp = "px";
+// let text = 0;
+// let mouve =function () {
+//     window.setInterval(function () {
+//         let int = 1;
+//         temp+=10;
+//         let dex=temp.toString()
+//         int += 1;
+//         span[int].style.left =dex+zemp;
+//     }, 10)
+
+// }
+
+// btn.addEventListener("click", mouve)
+
+ let btn = document.getElementsByTagName("button");
+ let body1=document.querySelector("#body1");
+ let body2=document.querySelector("#body2");
+
+ console.log(body1);
+ console.log(body2);
+
+
+ 
+// let remove=()=>{
+
+// }
+
+
+//  btn.addEventListener("click",)
+
+
